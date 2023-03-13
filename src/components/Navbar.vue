2c726ae1b85054ee9f5e5761ce3cc9f8998cb6fe
Takeyuki UEDA <ueda@latelierdueda.com>
2023-03-13 12:16:03 +0900

    for Vuetify3 notation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,7 +40,7 @@
         <!-- v-list-tile is changed to v-list-item -->
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action v-if="!link.svg">
-            <v-icon class="white--text">{{ link.icon }}</v-icon>
+            <v-icon class="white--text" :icon="link.icon"></v-icon>
           </v-list-item-action>
 
           <v-list-item-action v-if="link.svg" class="white--text">
